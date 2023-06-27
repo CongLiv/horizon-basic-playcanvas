c7ed20a60ee3b69365a0c2f828e612f8c43423a4
Cong Vu <congliv.nitc.neu@gmail.com>
2023-06-28 00:00:16 +0700

    update camera movement

--- src/script/follow.js
+++ src/script/follow.js
@@ -18,7 +18,7 @@ export function Follow() {
     this.following = false; // Biến theo dõi trạng thái follow
     this.smoothTime = 1; // Thời gian mượn camera
     this.velocity = new pc.Vec3(); // Vận tốc của camera
-    this.lastPos = new pc.Vec3(); // Vị trí tạm thời của camera
+    this.lastX = 0; // Vị trí x cuối cùng của camera
   };
 
   // update code called every frame
@@ -35,9 +35,13 @@ export function Follow() {
     // Kiểm tra xem target có di chuyển thẳng hay không
     var isMovingStraight = x == 0;
 
+    if (isMovingStraight){
+      this.lastX = this.entity.getPosition().x;
+    }
     // Nếu target đang di chuyển thẳng và chưa đang theo dõi, bắt đầu follow
     if (isMovingStraight && !this.following) {
       this.following = true;
+      this.lastX = this.entity.getPosition().x;
     }
 
     // Nếu target đang di chuyển trái hoặc phải và đang theo dõi, dừng follow
@@ -53,17 +57,23 @@ export function Follow() {
       var position = this.entity.getPosition().clone();
       position.lerp(position, targetPosition.add(offset), this.smoothTime * dt);
       this.entity.setPosition(position);
-      this.lastPos = position;
-    }
-    else {
+      this.lastX = this.entity.getPosition().x;
+      
+    } else  
+    {
+      // Nếu không theo dõi, camera chỉ di chuyển thẳng
       this.entity.translate(0, 0, this.targetEntity.speed * dt);
 
-      // if current pos is further 5 units from last pos, move camera temporarily
-      if (this.entity.getPosition().distance(this.lastPos) > 5) {
-        
-
+      // Nếu target di chuyển ra ngoài camera, camera sẽ di chuyển để đuổi kịp
+      if (Math.abs(this.lastX - this.targetEntity.getPosition().x) > 10) {
+        // camera move slightly to make sure the plane is in the view
+        let moveSpeed = this.lastX - this.targetEntity.getPosition().x > 0 ? -1 : 1;
+        this.entity.translate(
+          moveSpeed * this.targetEntity.speed * dt,
+          0,
+          0
+        );
       }
     }
-
   };
 }
