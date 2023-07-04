ca0bb95ebf21fa2d8402dbffb86ea29195da904c
Cong Vu <congliv.nitc.neu@gmail.com>
2023-07-04 23:43:01 +0700

    - update script ui folder

---
+++ src/script/ui/endUI/endBackgroundScript.js
@@ -0,0 +1,32 @@
+import { PlayScene } from "../../../scenes/playScene";
+import { SceneManager } from "../../../templates/scene/sceneManager";
+
+export function EndBackgroundScript() {
+  var endBackgroundScript = new pc.createScript("endBackgroundScript");
+
+  endBackgroundScript.attributes.add("player", {
+    type: "entity",
+    title: "Player",
+  });
+
+  endBackgroundScript.prototype.initialize = function () {
+    this._explodeCounter = 0;
+  };
+
+  endBackgroundScript.prototype.update = function (dt) {
+
+    this._explodeCounter += dt;
+
+    if (this._explodeCounter >= 0.5){
+      // make background opacity increase slowly
+      let opacity = this.entity.element.opacity;
+      opacity += 0.01;
+      this.entity.element.opacity = opacity;
+  
+      if (opacity >= 1){
+          SceneManager.loadScene(new PlayScene());
+      }
+    }
+
+  };
+}
