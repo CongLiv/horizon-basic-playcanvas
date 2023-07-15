8bdd62f0f53cf2c9b69e931c6b7cf0ed030d7142
Cong Vu <congliv.nitc.neu@gmail.com>
2023-07-15 18:47:20 +0700

    - update obj script

--- src/script/object/objectScript.js
+++ src/script/object/objectScript.js
@@ -11,13 +11,13 @@ export function ObjectScript(){
     });
 
     objectScript.prototype.initialize = function() {
-
+        this._destroyDistance = 300;
     }
 
     objectScript.prototype.update = function(dt) {
 
         // if object after player 100 unit, destroy object
-        if(this.entity.getPosition().z < this.player.getPosition().z - 300){
+        if(this.entity.getPosition().z < this.player.getPosition().z - this._destroyDistance){
             this.entity.destroyObject();
         }
 
