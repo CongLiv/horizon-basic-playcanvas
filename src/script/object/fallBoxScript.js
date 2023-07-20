61654aecc0b034461c309abc1eac7ff2d3825acd
Cong Vu <congliv.nitc.neu@gmail.com>
2023-07-20 22:30:19 +0700

    - update fall box random direction

--- src/script/object/fallBoxScript.js
+++ src/script/object/fallBoxScript.js
@@ -3,21 +3,28 @@ import { Game } from "../../game";
 export function FallBoxScript() {
   var fallBoxScript = pc.createScript("fallBoxScript");
 
+  fallBoxScript.attributes.add("direction", {
+    type: "number",
+    title: "Direction",
+    default: 1,
+  });
+
   fallBoxScript.prototype.initialize = function () {
     this._startFall = false;
     this._fallDistance = 75;
   };
 
   fallBoxScript.prototype.update = function (dt) {
-
-    let playerDistance = this.entity.getPosition().z - Game.player.getPosition().z;
+    let playerDistance =
+      this.entity.getPosition().z - Game.player.getPosition().z;
 
     if (playerDistance < this._fallDistance) {
       this._startFall = true;
     }
+    let fallDirection = this.direction;
 
-   if (this._startFall == true) {this.entity.rigidbody.applyForce(10, -10, 0);}
+    if (this._startFall == true) {
+      this.entity.rigidbody.applyForce(10 * fallDirection, -10, 0);
+    }
   };
-
-  
 }
--- src/templates/object/obstacle/fallBox.js
+++ src/templates/object/obstacle/fallBox.js
@@ -1,31 +1,32 @@
-
+import { Utils } from "../../../utils/utils.js";
 import { Box } from "../model/box.js";
 import { SuperObject } from "../superObject.js";
 
-export class FallBox extends SuperObject{
+export class FallBox extends SuperObject {
+  constructor() {
+    super();
+    this.tags.add("obstacle");
+    this.objectWidth = 40;
+  }
 
-    constructor(){
-        super();
-        this.tags.add("obstacle");
-        this.objectWidth = 40;
+  _createObject() {
+    let direction = Utils.getChance(0.5) ? 1 : -1;  // 1 for right, -1 for left
+    for (let i = -20; i <= 20; i += 10) {
+      let box = new Box(1.5, 20, 3);
+      box.model.material = this.OBSTACLE_MATERIAL;
+      box.addComponent("script");
+      box.script.create("fallBoxScript", {
+        attributes: {
+          direction: direction,
+        },
+      });
+      this.addChild(box);
+      box.setLocalPosition(i, 0, 0);
     }
+  }
 
-    _createObject(){
-      
-        for (let i = -20; i <= 20; i += 10) {
-            let box = new Box(1.5, 20, 3);
-            box.model.material = this.OBSTACLE_MATERIAL;
-            box.addComponent("script");
-            box.script.create("fallBoxScript");
-            this.addChild(box);
-            box.setLocalPosition(i, 0, 0);
-        }
-    }
-
-    spawnToPosition(position, spawnContainer){
-        this.setLocalPosition(position);
-        spawnContainer.addChild(this);
-    }
-
-
+  spawnToPosition(position, spawnContainer) {
+    this.setLocalPosition(position);
+    spawnContainer.addChild(this);
+  }
 }
